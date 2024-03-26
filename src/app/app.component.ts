3f36017e84fd23e83a2a24b803a95e4faec90c7e
Kilian LE DU <ledu.kilian@gmail.com>
2024-03-26 19:34:20 +0100

    Affichage liste pokémon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Pokemon } from "./pokemon";
+import { POKEMONS } from "./mock-pokemon-list";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Angular Pokémon';
-  pokemons = ['Bulbizarre', 'Salamèche', 'Carapuce'];
+  pokemonList: Pokemon[] = POKEMONS;
+
+  constructor() {
+
+  }
+
+  ngOnInit():void {
+    console.table(this.pokemonList);
+    this.selectPokemon(this.pokemonList[9]);
+  }
+
+  selectPokemon(pokemon: Pokemon): void {
+    console.log(`Vous avez choisi : ${pokemon.name}`);
+  }
 }
