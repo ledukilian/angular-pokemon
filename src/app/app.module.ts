07b3ac93a0148d7a90241256b56aca27341e8e83
Kilian LE DU <ledu.kilian@gmail.com>
2024-03-30 12:42:32 +0100

    Ajout directive bordure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {BorderCardDirective} from "./border-card.directive";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BorderCardDirective
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
