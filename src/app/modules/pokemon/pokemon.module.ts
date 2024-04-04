efa6a3fdef58bfe76014cf318193abad170690aa
Kilian LE DU <ledu.kilian@gmail.com>
2024-04-04 13:51:45 +0200

    Refacto archi (découpage en module)

---
+++ src/app/modules/pokemon/pokemon.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListPokemonComponent } from "./components/pages/list-pokemon/list-pokemon.component";
+import { DetailPokemonComponent } from "./components/pages/detail-pokemon/detail-pokemon.component";
+import { RouterModule, Routes } from "@angular/router";
+import { BorderCardDirective } from "./directives/border-card/border-card.directive";
+import { PokemonTypeColorPipe } from "./pipes/pokemon-type-color/pokemon-type-color.pipe";
+import { PokemonTypeEmojiPipe } from "./pipes/pokemon-type-emoji/pokemon-type-emoji.pipe";
+
+const pokemonRoutes: Routes = [
+  { path: 'pokemons', component: ListPokemonComponent },
+  { path: 'pokemons/:id', component: DetailPokemonComponent }
+];
+
+@NgModule({
+  declarations: [
+    ListPokemonComponent,
+    DetailPokemonComponent,
+    BorderCardDirective,
+    PokemonTypeColorPipe,
+    PokemonTypeEmojiPipe
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(pokemonRoutes)
+  ]
+})
+export class PokemonModule { }
